3affba8fee682ded533f0d0effdcb3c239fca982
hochjager <81619961+hochjager@users.noreply.github.com>
2021-07-11 15:28:22 +0300

    Add a greeting

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
 # A tic-tac-toe game that can be played in the terminal
+greeting = """Welcome to Tic-Tac-Toe! 
+It is a game for two players, X and O, who take turns marking the spaces in a 3×3 grid. 
+The player who succeeds in placing three of their marks in a diagonal, horizontal, or vertical row is the winner.
+
+More info can be found on https://en.wikipedia.org/wiki/Tic-tac-toe"""
+print(greeting)

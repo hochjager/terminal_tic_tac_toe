ca4c920c5e97565a38d46c4eca2b70f20eb7d9d9
hochjager <81619961+hochjager@users.noreply.github.com>
2021-07-13 17:27:15 +0300

    Deleted Functions.py

--- Functions.py
+++
@@ -1,9 +0,0 @@
-# Greeting
-def greeting():
-    return """              Welcome to Tic-Tac-Toe! 
-    
-    This simple game is for two players, X and O, who take turns marking the spaces in a 3×3 grid. 
-    The player who succeeds in placing three of their marks in a diagonal, horizontal, or vertical row is the winner.
-    
-    More info can be found on https://en.wikipedia.org/wiki/Tic-tac-toe
-    """

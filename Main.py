6feaa7ce24308700dc9ca55d7f3551a0f4cc8faf
hochjager <81619961+hochjager@users.noreply.github.com>
2021-07-13 17:03:14 +0300

    The grid works now. Create a separate file where functions are stored

--- Main.py
+++ Main.py
@@ -1,14 +1,9 @@
 # A tic-tac-toe game that can be played in the terminal
 
-# Greeting
-greeting = """              Welcome to Tic-Tac-Toe! 
+import Functions as f
 
-This simple game is for two players, X and O, who take turns marking the spaces in a 3×3 grid. 
-The player who succeeds in placing three of their marks in a diagonal, horizontal, or vertical row is the winner.
-
-More info can be found on https://en.wikipedia.org/wiki/Tic-tac-toe
-"""
-print(greeting)
+# Print out a greeting
+print(f.greeting())
 
 # Ask for player names and save them
 players = {}
@@ -19,37 +14,41 @@ for each in range(1, 3):
     print(f"You will be playing using {marks[each]}.\n")
 print("Let's play now!")
 
-# Create a grid of 9 cells with " " (empty) values
-# grid_cells_nums = [each for each in range(0, 9)]
-# grid = dict.fromkeys(grid_cells_nums, "")
-base_grid = \
+# Create a dictionary that will represent grid cells (str) and their values (int)
+grid = {}
+for each in range(1, 10):
+    grid[f"{each}"] = each
+print(grid)
+
+
+# Draw a grid
+def base_grid(grid_values):
+    return \
+        f"""
+      {grid_values["1"]} | {grid_values["2"]} | {grid_values["3"]}
+     --- --- ---
+      {grid_values["4"]} | {grid_values["5"]} | {grid_values["6"]}
+     --- --- ---
+      {grid_values["7"]} | {grid_values["8"]} | {grid_values["9"]}
     """
-  1 | 2 | 3
- --- --- ---
-  4 | 5 | 6 
- --- --- ---
-  7 | 8 | 9 
-"""
-print(base_grid)
 
 
 # Game mechanics. Checking turns and prompting players to insert X or O, depending on player's turn.
-def game_switcher(turn):
+def turn_switcher(turn):
     switcher = turn % 2 + 1
     print(f"{players[switcher]}, please, type a number of a cell where you want to put your mark {marks[switcher]}.")
     marked_cell = input()
-    if marked_cell in base_grid:
-        base_grid.replace(marked_cell, marks[switcher])
-        print(base_grid)
-        base_grid = base_grid
+    temp_grid = base_grid(grid)
+    if marked_cell in temp_grid:
+        grid[marked_cell] = marks[switcher]
+        return base_grid(grid)
     else:
         print("Wrong character.")
-        game_switcher(turn)
+        turn_switcher(turn)
 
 
-for turn in range(0, 9):
-    game_switcher(turn)
-    print(base_grid)
+print(base_grid(grid))
 
-#     marked_cell = input()
-# else:
+for turns in range(0, 9):
+    updated_grid = turn_switcher(turns)
+    print(updated_grid)
